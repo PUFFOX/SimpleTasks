57b42a6391e24ee6be3ba2f6576f586fa4381a95
puffox <puffoxx@gmail.com>
2026-04-26 16:21:29 +0300

    Add project files.

---
+++ SimpleTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using SimpleTask.TasksConsole;
+using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace SimpleTask
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            while (true)
+            {
+                Console.WriteLine("================================\n");
+                Console.WriteLine("Select a task:");
+                Console.WriteLine("1 - Translate text");
+                Console.WriteLine("0 - Exit");
+                Console.WriteLine("--------------------------------\n");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Exiting...");
+                        return;
+
+                    case 1:
+                        KeyboardConverter.Run();
+                        break;
+
+
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ SimpleTask/TasksConsole/KeyboardConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTask.TasksConsole
+{
+    public class KeyboardConverter
+    {
+
+        public static void Run()
+        {
+            Console.WriteLine("================================\n");
+            Console.WriteLine("Keyboard Converter - English to Ukrainian");
+            const string en = "qwertyuiop[]asdfghjkl;'zxcvbnm,./?";
+            const string ua = "йцукенгшщзхїфівапролджєячсмитьбю.,";
+            do
+            {
+                Console.WriteLine("Enter text to translate:");
+                Console.WriteLine("0 - Back to menu");
+
+                string input = Console.ReadLine().ToLower();
+                char[] inputChars = input.ToCharArray();
+
+                if(input == "0")
+                {
+                    return;
+                }
+
+                for (int i = 0; i < inputChars.Length; i++)
+                {
+                    int index = en.IndexOf(inputChars[i]);
+                    if (index >= 0)
+                    {
+                        inputChars[i] = ua[index];
+                    }
+                }
+
+                Console.WriteLine(new string(inputChars));
+                Console.WriteLine("================================\n");
+
+
+            }
+            while (true);
+            
+
+        }
+    }
+}
+
